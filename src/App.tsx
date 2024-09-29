683067b8c95e89aeae25df51e044fa73c9b1b9ba
innkeeping <innkeeping@metagame.wtf>
2024-09-29 04:54:57 -0500

    sharpen left edges, book like rounds 📚📑

--- src/App.tsx
+++ src/App.tsx
@@ -134,55 +134,55 @@ function App() {
           Bigger picture, the grander game.
         </h2>
       </div>
-      <div className="container mx-auto p-4 mt-30">
+      <div className="container mx-auto p-4 mt-30 gap-4">
       <div className="mb-16">
         <Carousel responsive={responsive} className="gap-4 md:gap-6 lg:gap-8">
-          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4 gap-4">
+          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4 rounded-l-none rounded-rt-lg rounded-br-lg">
             <figure>
               <img src="./assets/gameb.webp" alt="drw" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title text-white z-10">WTF is GameB?</h2>
+              <h2 className="card-title text-white z-10 font-extrabold">WTF is GameB?</h2>
             </div>
           </div>
-          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4">
+          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4 rounded-l-none rounded-rt-lg rounded-br-lg">
             <figure>
               <img src="./assets/metacrisis.webp" alt="drw" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title text-white z-10">WTF is the MetaCrisis?</h2>
+              <h2 className="card-title text-white z-10 font-extrabold">WTF is the MetaCrisis?</h2>
             </div>
           </div>
-          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4">
+          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4 rounded-l-none rounded-rt-lg rounded-br-lg">
             <figure>
               <img src="./assets/moloch.webp" alt="drw" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title text-white z-10">Meditations on Moloch</h2>
+              <h2 className="card-title text-white z-10 font-extrabold">Meditations on Moloch</h2>
             </div>
           </div>
-          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4">
+          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4 rounded-l-none rounded-rt-lg rounded-br-lg">
             <figure>
               <img src="./assets/metamodernist.webp" alt="drw" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title text-white z-10 font-bold">Political MetaModernism:Bridging Divides</h2>
+              <h2 className="card-title text-white z-10 font-extrabold">Political MetaModernism:Bridging Divides</h2>
             </div>
           </div>
-          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4">
+          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4 rounded-l-none rounded-rt-lg rounded-br-lg">
             <figure>
               <img src="./assets/goodquests.webp" alt="drw" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title text-white z-10">Choose Good Quests</h2>
+              <h2 className="card-title text-white z-10 font-extrabold">Choose Good Quests</h2>
             </div>
           </div>
-          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4">
+          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4 rounded-l-none rounded-rt-lg rounded-br-lg">
             <figure>
               <img src="./assets/memedriven.webp" alt="drw" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title text-white z-10">Meme Driven Organizations</h2>
+              <h2 className="card-title text-white z-10 font-extrabold">Meme Driven Organizations</h2>
             </div>
           </div>
         </Carousel>
@@ -190,59 +190,59 @@ function App() {
         <div className="mb-16">
           <div className="justify-start">
               <h1 className="text-2xl font-medium text-primary justify-left text-left ml-12">
-                 Web3 General
+                Web3 General
               </h1>
               <h2 className="text-xl font-medium text-accent justify-left text-left ml-12">
                 Web3 basics: how & why
               </h2>
             </div>
         <Carousel responsive={responsive} className="gap-4 md:gap-6 lg:gap-8">
-          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4">
+          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4 rounded-l-none rounded-rt-lg rounded-br-lg">
             <figure>
               <img src="./assets/ethereum.webp" alt="wtf is ethereum" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title text-white z-10">WTF is Ethereum?</h2>
+              <h2 className="card-title text-white z-10 font-extrabold">WTF is Ethereum?</h2>
             </div>
           </div>
-          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4">
+          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4 rounded-l-none rounded-rt-lg rounded-br-lg">
             <figure>
               <img src="./assets/daos.webp" alt="wtf are daos" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title text-white z-10">WTF are DAOs?</h2>
+              <h2 className="card-title text-white z-10 font-extrabold">WTF are DAOs?</h2>
             </div>
           </div>
-          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4">
+          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4 rounded-l-none rounded-rt-lg rounded-br-lg">
             <figure>
               <img src="./assets/defi.webp" alt="wtf is defi" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title text-white z-10">WTF is DeFi?</h2>
+              <h2 className="card-title text-white z-10 font-extrabold">WTF is DeFi?</h2>
             </div>
           </div>
-          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4">
+          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4 rounded-l-none rounded-rt-lg rounded-br-lg">
             <figure>
               <img src="./assets/nfts.webp" alt="wtf are nfts" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title text-white z-10">WTF are NFTs?</h2>
+              <h2 className="card-title text-white z-10 font-extrabold">WTF are NFTs?</h2>
             </div>
           </div>
-          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4">
+          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4 rounded-l-none rounded-rt-lg rounded-br-lg">
             <figure>
               <img src="./assets/dapps.webp" alt="wtf are dapps" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title text-white z-10">WTF are dApps?</h2>
+              <h2 className="card-title text-white z-10 font-extrabold">WTF are dApps?</h2>
             </div>
           </div>
-          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4">
+          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4 rounded-l-none rounded-rt-lg rounded-br-lg">
             <figure>
               <img src="./assets/web3builder.webp" alt="web 3 developer" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title text-white z-10">Become a Web3 Developer</h2>
+              <h2 className="card-title text-white z-10 font-extrabold">Become a Web3 Developer</h2>
             </div>
           </div>
         </Carousel>
@@ -255,64 +255,36 @@ function App() {
       </div>
         <div className="mb-16">
         <Carousel responsive={responsive} className="gap-4 md:gap-6 lg:gap-8">
-          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4">
+          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4 rounded-l-none rounded-rt-lg rounded-br-lg">
             <figure>
               <img src="./assets/best-dao-contributor.webp" alt="Become the Best Contributor in any DAO" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title text-white z-10">Become the Best Contributor in any DAO</h2>
+              <h2 className="card-title text-white z-10 font-extrabold">Become the Best Contributor in any DAO</h2>
             </div>
           </div>
-          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4">
+          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4 rounded-l-none rounded-rt-lg rounded-br-lgS">
             <figure>
               <img src="./assets/daosummoner.webp" alt="Starting DAOs 101" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title text-white z-10">Starting DAOs 101</h2>
+              <h2 className="card-title text-white z-10 font-extrabold">Starting DAOs 101</h2>
             </div>
           </div>
-          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4">
+          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4 rounded-l-none rounded-rt-lg rounded-br-lg">
             <figure>
               <img src="./assets/impact-networks.webp" alt="Impact Networks" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title text-white z-10">Impact Networks</h2>
+              <h2 className="card-title text-white z-10 font-extrabold">Impact Networks</h2>
             </div>
           </div>
-          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4">
+          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4 rounded-l-none rounded-rt-lg rounded-br-lg">
             <figure>
               <img src="./assets/coordinape.webp" alt="Use Coordinape to Reward Contributors" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title text-white z-10">Use Coordinape to Reward Contributors</h2>
-            </div>
-            <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4">
-            <figure>
-              <img src="./assets/holacracy.webp" alt="Effective Meetings with Holacracy" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title text-white z-10">Effective Meetings with Holacracy</h2>
-            </div>
-            <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4">
-            <figure>
-              <img src="./assets/daowriter.webp" alt="Entering DAOs as a Writer" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title text-white z-10">Use Coordinape to Reward Contributors</h2>
-            </div>
-            <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4">
-            <figure>
-              <img src="./assets/successful-teams.webp" alt="Building Successful Teams" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title text-white z-10">Building Successful Teams</h2>
-            </div>
-            <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4">
-            <figure>
-              <img src="./assets/coordinape.webp" alt="Use Coordinape to Reward Contributors" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title text-white z-10">Use Coordinape to Reward Contributors</h2>
+              <h2 className="card-title text-white z-10 font-extrabold">Use Coordinape to Reward Contributors</h2>
             </div>
           </div>
         </Carousel>
@@ -326,39 +298,47 @@ function App() {
         <div className="mb-16">
         <Carousel responsive={responsive} className="gap-4 md:gap-6 lg:gap-8">
         <div className="mb-8">
-          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4">
+          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4 rounded-l-none rounded-rt-lg rounded-br-lg">
             <figure>
-              <img src="./assets/buildonop.png" alt="drw" />
+              <img src="./assets/creatorgame.webp" alt="How to Play the Creator Game" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title text-white z-10">Playbook 13</h2>
+              <h2 className="card-title text-white z-10 font-extrabold">How to Play the Creator Game</h2>
             </div>
           </div>
           </div>
-          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4">
+          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4 rounded-l-none rounded-rt-lg rounded-br-lg">
             <figure>
-              <img src="./assets/buildonop.png" alt="drw" />
+              <img src="./assets/time.webp" alt="Embrace the Ticking Clock" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title text-white z-10">Playbook 14</h2>
+              <h2 className="card-title text-white z-10 font-extrabold">Embrace the Ticking Clock</h2>
             </div>
           </div>
-          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4">
+          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4 rounded-l-none rounded-rt-lg rounded-br-lg">
             <figure>
-              <img src="./assets/buildonop.png" alt="drw" />
+              <img src="./assets/biases.webp" alt="Know your Biases" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title text-white z-10">Playbook 15</h2>
+              <h2 className="card-title text-white z-10 font-extrabold">Know your BiasesS</h2>
             </div>
           </div>
-          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4">
+          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4 rounded-l-none rounded-rt-lg rounded-br-lg">
             <figure>
-              <img src="./assets/buildonop.png" alt="drw" />
+              <img src="./assets/imposter.webp" alt="Understanding Imposter Syndrome" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title text-white z-10">Playbook 16</h2>
+              <h2 className="card-title text-white z-10 font-extrabold">Understanding Imposter Syndrome</h2>
             </div>
           </div>
+          <div className="basis-1/4 md:basis-1/2 xl:basis-1/3 2xl:basis-1/4 flex-1 flex place-items-center p-0 image-full z-0 card bg-base-100 shadow-xl mr-4 rounded-l-none rounded-rt-lg rounded-br-lg">
+  <figure>
+    <img src="./assets/journaling.webp" alt="Journaling like Dickie Bush" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title text-white z-10 font-extrabold">Journaling like Dickie Bush</h2>
+  </div>
+</div>
         </Carousel>
         </div>
       </div>
