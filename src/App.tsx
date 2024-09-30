dd619eac832c5bfafdc3f8be66f5abb95e696142
innkeeping <innkeeping@metagame.wtf>
2024-09-30 17:56:32 -0500

    update navbar component 🦕

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from'react';
 import './App.css';
 import Carousel from'react-multi-carousel';
 import'react-multi-carousel/lib/styles.css';
+import Navbar from './components/Navbar';
 
 const responsive = {
   superLargeDesktop: {
@@ -25,102 +26,7 @@ const responsive = {
 function App() {
   return (
     <>
-      <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2 mb-4">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
-              </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a>Support</a>
-              </li>
-              <li>
-                <a>Categories</a>
-                <ul className="p-2">
-                <li>
-                    <a>Meta - Game-b</a>
-                  </li>
-                  <li>
-                    <a>Web3 General</a>
-                  </li>
-                  <li>
-                    <a>DAO Playbooks</a>
-                  </li> 
-                  <li>
-                    <a>Self Actualization</a>
-                  </li>
-                  <li>
-                    <a>Building Chain by Chain</a>
-                  </li>
-                  <li>
-                    <a>ReFi</a>
-                  </li>
-                  <li>
-                    <a>Random Skills</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Submit</a>
-              </li>
-            </ul>
-          </div>
-          <a className="btn btn-ghost text-xl">dAcademy</a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Support</a>
-            </li>
-            <li>
-              <details>
-                <summary>Categories</summary>
-                <ul className="p-2 z-10">
-                  <li>
-                    <a>Meta - Game-b</a>
-                  </li>
-                  <li>
-                    <a>Web3 General</a>
-                  </li>
-                  <li>
-                    <a>DAO Playbooks</a>
-                  </li> 
-                  <li>
-                    <a>Self Actualization</a>
-                  </li>
-                  <li>
-                    <a>Building Chain by Chain</a>
-                  </li>
-                  <li>
-                    <a>ReFi</a>
-                  </li>
-                  <li>
-                    <a>Random Skills</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Submit</a>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-end px-4">
-          <w3m-button className="btn btn-sm pr-16 rounded" />
-        </div>
-      </div>
+      <Navbar />
       <div>
       <h2 className="text-accent font-light text-3xl mb-16 pt-6">A de-store of knowledge.  Onboard forward.</h2>
       </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,98 +1,104 @@
-import React from 'react';
+import React from'react';
 
-<div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2 mb-4">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
-              </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+function Navbar() {
+  return (
+    <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2 mb-4">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              <li>
-                <a>Support</a>
-              </li>
-              <li>
-                <a>Categories</a>
-                <ul className="p-2">
-                <li>
-                    <a>Meta - Game-b</a>
-                  </li>
-                  <li>
-                    <a>Web3 General</a>
-                  </li>
-                  <li>
-                    <a>DAO Playbooks</a>
-                  </li> 
-                  <li>
-                    <a>Self Actualization</a>
-                  </li>
-                  <li>
-                    <a>Building Chain by Chain</a>
-                  </li>
-                  <li>
-                    <a>ReFi</a>
-                  </li>
-                  <li>
-                    <a>Random Skills</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Submit</a>
-              </li>
-            </ul>
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+            </svg>
           </div>
-          <a className="btn btn-ghost text-xl">dAcademy</a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
             <li>
               <a>Support</a>
             </li>
             <li>
-              <details>
-                <summary>Categories</summary>
-                <ul className="p-2 z-10">
-                  <li>
-                    <a>Meta - Game-b</a>
-                  </li>
-                  <li>
-                    <a>Web3 General</a>
-                  </li>
-                  <li>
-                    <a>DAO Playbooks</a>
-                  </li> 
-                  <li>
-                    <a>Self Actualization</a>
-                  </li>
-                  <li>
-                    <a>Building Chain by Chain</a>
-                  </li>
-                  <li>
-                    <a>ReFi</a>
-                  </li>
-                  <li>
-                    <a>Random Skills</a>
-                  </li>
-                </ul>
-              </details>
+              <a>Categories</a>
+              <ul className="p-2">
+                <li>
+                  <a>Meta - Game-b</a>
+                </li>
+                <li>
+                  <a>Web3 General</a>
+                </li>
+                <li>
+                  <a>DAO Playbooks</a>
+                </li>
+                <li>
+                  <a>Self Actualization</a>
+                </li>
+                <li>
+                  <a>Building Chain by Chain</a>
+                </li>
+                <li>
+                  <a>ReFi</a>
+                </li>
+                <li>
+                  <a>Random Skills</a>
+                </li>
+              </ul>
             </li>
             <li>
               <a>Submit</a>
             </li>
           </ul>
         </div>
-        <div className="navbar-end px-4">
-          <w3m-button className="btn btn-sm pr-16 rounded" />
-        </div>
+        <a className="btn btn-ghost text-xl">dAcademy</a>
       </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Support</a>
+          </li>
+          <li>
+            <details>
+              <summary>Categories</summary>
+              <ul className="p-2 z-10">
+                <li>
+                  <a>Meta - Game-b</a>
+                </li>
+                <li>
+                  <a>Web3 General</a>
+                </li>
+                <li>
+                  <a>DAO Playbooks</a>
+                </li>
+                <li>
+                  <a>Self Actualization</a>
+                </li>
+                <li>
+                  <a>Building Chain by Chain</a>
+                </li>
+                <li>
+                  <a>ReFi</a>
+                </li>
+                <li>
+                  <a>Random Skills</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+          <li>
+            <a>Submit</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end px-4">
+        <w3m-button className="btn btn-sm pr-16 rounded" />
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
