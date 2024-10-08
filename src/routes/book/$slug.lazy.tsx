33e9b3ba063bf4ff944b10d37aa3cc14002e9d4e
δυς <d+gh@dhappy.org>
2024-10-08 04:29:07 -0400

    sorting capitalization errors preventing results 🛣️

--- src/routes/book/$slug.lazy.tsx
+++ src/routes/book/$slug.lazy.tsx
@@ -1,18 +1,19 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
 import { useState } from'react'
+import { createLazyFileRoute } from '@tanstack/react-router'
+import { useQuery } from '@tanstack/react-query'
+import { gql, request } from 'graphql-request'
 import Chapters from '../../components/chapters'
 import Content from '../../components/content'
 import Reward from '../../components/reward'
 import playbooks from '../../playbooks.json'
 import { toSlug } from '../../components/CarouselItem'
-import { gql, request } from 'graphql-request'
-import { useQuery } from '@tanstack/react-query'
 
-
-
-const questChainQueryDocument = (
-  gql`query ChainDetails($name: String) {
-    questChains(where: {name: $name}) {
+const questChainQueryDocument = gql`
+  query ChainDetails($name: String!) {
+    questChains(where: {
+      name_starts_with_nocase: $name,
+      name_ends_with_nocase: $name
+    }) {
       description
       imageUrl
       quests(orderBy: questId) {
@@ -21,67 +22,101 @@ const questChainQueryDocument = (
         description
       }
     }
-  }`
-)
+  }
+`
+
+export type Quest = {
+  questId: number
+  name: string
+  description: string
+}
+export type Chain = {
+  description: string
+  imageUrl: string
+  quests: Array<Quest>
+}
+export type GraphChainResponse = {
+  questChains: Array<Chain>
+}
 
 export const Route = createLazyFileRoute('/book/$slug')({
   component: Book,
 })
 
-console.log({playbooks})
-
 function Book() {
-  const { slug } = Route.useParams()
-  const book = playbooks.map(({ books }) => books).flat().find(({ title }) => (
-    toSlug(title) === slug
-  ))
-  if(!book){
-    return(
-      <h1>No book found: "{slug}"</h1>
-    )
-  }
-  const { data } = useQuery({
-    queryKey: [`chain-${slug}`],
-    queryFn: () => (
-      request(
-        'https://api.studio.thegraph.com/query/71457/quest-chains-optimism/version/latest',
-        questChainQueryDocument,
-        { name: book.title },
+  try {
+    const { slug } = Route.useParams()
+    const book = (
+      playbooks.map(({ books }) => books)
+      .flat()
+      .find(
+        ({ title }) => toSlug(title) === slug
       )
     )
-  })
-  console.log({book, data})
-  const [content, setContent] = useState('');
-  const [activeQuest, setActiveQuest] = useState(1);
-  const chapterSelected = (index: number) => {
-    setActiveQuest(index + 1);
-    switch (index) {
-      case 0:
-        setContent('Content for Quest 1');
-        break;
-      case 1:
-        setContent('Content for Quest 2');
-        break;
-      case 2:
-        setContent('Content for Quest 3');
-        break;
-      default:
-        setContent('');
-    }
-  };
 
-  return (
-    <>
-      <div className="container mx-auto p-20">
-        <p className="text-sm text-secondary mt-5 text-left pl-1">Author Wallet: 0x1234567890abcdef</p>
-        <h1 className="text-4xl md:text-6xl font-bold text-left mt-2">{book.title}</h1>
-        <p className="text-sm text-white text-left pl-1 mt-6 mb-4">Last updated: insert date</p>
-      <main className="md:flex justify-start">
-        <Chapters onChange={chapterSelected} active={activeQuest}/>
-        <Content content={content} active={activeQuest}/>
-        <Reward />
-      </main>
-    </div>
-    </>
-  );
+    const {
+      data: { questChains: [chain] = [] } = {},
+      error,
+      isLoading,
+    } = (
+      useQuery<GraphChainResponse>({
+        queryKey: [`chain-${slug}`],
+        queryFn: async () => (
+          request(
+            'https://api.studio.thegraph.com/query/71457/quest-chains-optimism/version/latest',
+            questChainQueryDocument,
+            { name: book?.title },
+          )
+        )
+      })
+    )
+
+    const [_content, setContent] = useState('');
+    const [activeQuest, setActiveQuest] = useState(1);
+    const chapterSelected = (index: number) => {
+      setActiveQuest(index + 1);
+      switch (index) {
+        case 0:
+          setContent('Content for Quest 1');
+          break;
+        case 1:
+          setContent('Content for Quest 2');
+          break;
+        case 2:
+          setContent('Content for Quest 3');
+          break;
+        default:
+          setContent('');
+      }
+    }
+
+    if(error) throw error
+    if(!book) {
+      throw new Error(`No book found for: "${slug}".`)
+    }
+    if(!chain) {
+      throw new Error(
+        `No chain found for: "${slug}" = "${book.title}".`
+      )
+    }
+
+    if(isLoading) return <h1>Loading…</h1>
+
+    return (
+      <>
+        <div className="container mx-auto p-20">
+          <p className="text-sm text-secondary mt-5 text-left pl-1">Author Wallet: 0x1234567890abcdef</p>
+          <h1 className="text-4xl md:text-6xl font-bold text-left mt-2">{book.title}</h1>
+          <p className="text-sm text-white text-left pl-1 mt-6 mb-4">Last updated: insert date</p>
+          <main className="md:flex justify-start">
+            <Chapters onChange={chapterSelected} active={activeQuest}/>
+            <Content content={chain.description} active={activeQuest}/>
+            <Reward />
+          </main>
+        </div>
+      </>
+    )
+  } catch (error) {
+    return <h1>Error: {(error as Error).message}</h1>
+  }
 }
