30f88f731b5293fc355215e568f4d2b03289985f
innkeeping <innkeeping@metagame.wtf>
2024-10-17 04:46:19 -0500

    foot full 🦶🦶

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,7 +2,7 @@ import { createRootRoute, Outlet } from '@tanstack/react-router'
 // import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import Navbar from '../components/Navbar'
 import '../App.css'
-import Footer from '../components/Footer'
+// import Footer from '../components/Footer'
 
 export const Route = createRootRoute({
   component: () => {
