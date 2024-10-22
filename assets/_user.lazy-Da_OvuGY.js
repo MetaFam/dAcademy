9dfdd06471f547b20c488cc239cb3ad411067b41
innkeeping <innkeeping@metagame.wtf>
2024-10-22 16:54:46 -0500

    Updates

---
+++ assets/_user.lazy-Da_OvuGY.js
@@ -0,0 +1,28 @@
+import{j as e,e as x,f as i,L as m}from"./index-B3nH8WrL.js";import{C as u,r as h}from"./index-vUgOGBE5.js";import{g as l,u as o,r as c,c as p}from"./clsx-CICJCmT4.js";import{t as j}from"./utils-DE92Morm.js";import{T as f}from"./Top-DyE3cSIx.js";import{u as b}from"./useUsername-hbu8_si_.js";import"./isAddressEqual-CYfaHVHF.js";import"./secp256k1-Jr9fFTYw.js";const N=l`
+  query ChainDetails($address: String) {
+    user(id: $address) {
+      completedQuestChains {
+        token {
+          imageUrl
+        }
+        name
+      }
+    }
+  }
+`,y=({account:t})=>{const{data:{user:{completedQuestChains:a}}}=o({queryKey:[`completed-${t}`],queryFn:async()=>c("https://api.studio.thegraph.com/query/71457/quest-chains-optimism/version/latest",N,{address:t==null?void 0:t.toLowerCase()})});return e.jsx(u,{responsive:h,className:"top-0 gap-4 md:gap-6 lg:gap-8 w-full mr-0",children:a==null?void 0:a.map(r=>e.jsxs("div",{className:"card bg-secondary/25 h-auto max-w-md mr-4 mx-auto rounded-sm",children:[e.jsx("div",{className:"card-body items-center text-center",children:e.jsx("div",{className:"tooltip cursor-default","data-tip":r.name,children:e.jsx("h2",{className:"card-title text-ellipsis overflow-hidden line-clamp-1",children:r.name})})}),e.jsx("figure",{className:"px-2 pt-2 pb-4",children:e.jsx("img",{src:j(r.token.imageUrl),alt:"Completion NFT",className:"rounded-sm"})})]},r.name))})},g=l`
+query ChainDetails($address: String) {
+  proofSubmissions(where: {user: $address}) {
+    description
+		timestamp
+		questChain {
+      name
+    }
+		questStatus {
+      status
+    }
+    quest {
+      name
+    }
+  }
+}
+`,w=({account:t})=>{const{data:{proofSubmissions:a}={}}=o({queryKey:[`submissions-${t}`],queryFn:async()=>c("https://api.studio.thegraph.com/query/71457/quest-chains-optimism/version/latest",g,{address:t==null?void 0:t.toLowerCase()})}),r=a==null?void 0:a.sort((s,d)=>{if(s.questStatus.status!==d.questStatus.status){const n=["review","fail","pass"];return n.indexOf(s.questStatus.status)-n.indexOf(d.questStatus.status)}return s.timestamp-d.timestamp});return e.jsxs("table",{className:"table",children:[e.jsx("thead",{className:"max-md:hidden",children:e.jsxs("tr",{children:[e.jsx("th",{children:"Date"}),e.jsx("th",{children:"Book"}),e.jsx("th",{children:"Chapter"}),e.jsx("th",{children:"Status"})]})}),e.jsx("tbody",{children:r==null?void 0:r.map(s=>e.jsxs("tr",{className:"max-md:border-current hover:bg-secondary/10",children:[e.jsx("td",{className:"max-md:list-item max-md:p-0 max-md:before:content-['Date:'] max-md:before:mr-1 max-md:before:font-bold max-md:mt-2",children:new Date(s.timestamp*1e3).toLocaleDateString()}),e.jsx("td",{className:"max-md:list-item max-md:p-0 max-md:before:content-['Book:'] max-md:before:mr-1 before:font-bold italic max-md:before:not-italic",children:s.questChain.name}),e.jsx("td",{className:"max-md:list-item max-md:p-0 max-md:before:content-['Chapter:'] max-md:before:mr-1 max-md:before:font-bold max-md:-indent-2 max-md:pl-2",children:s.quest.name}),e.jsx("td",{className:p((()=>{switch(s.questStatus.status){case"pass":return"text-success";case"fail":return"text-error";case"review":return"text-info";default:return"text-yellow-200"}})(),"max-md:list-item max-md:p-0 max-md:mb-4 max-md:before:content-['Status:'] max-md:before:mr-1 max-md:before:font-bold"),children:s.questStatus.status})]}))})]})},q=()=>e.jsx("div",{className:"card bg-secondary/25 h-auto w-full mr-4 mx-auto rounded-sm",children:e.jsxs("div",{className:"card-body items-center text-center",children:[e.jsx("h2",{className:"card-title",children:"Workshops Attended"}),e.jsx("p",{children:"From Org"}),e.jsx("p",{children:"Date Attended"}),e.jsx("p",{children:"Topic Reference"})]})}),v=x("/user/$user")({component:()=>{const{user:t}=v.useParams(),{address:a,ens:r,error:s}=b(t);return s?e.jsx("h1",{children:s}):e.jsxs("div",{children:[e.jsx("h1",{id:"top",className:"text-2xl font-semibold text-secondary mt-12 mb-8 scroll-m-28",children:"User Profile"}),e.jsxs("div",{className:"drawer lg:drawer-open",children:[e.jsx("input",{id:"my-drawer-2",type:"checkbox",className:"drawer-toggle"}),e.jsxs("div",{className:"drawer-content flex flex-col items-center justify-start",children:[e.jsxs("div",{id:"nfts-earned",className:"w-11/12 bg-secondary/25 scroll-m-24",children:[e.jsx("h1",{className:"text-lg font-semibold mt-4 mb-4",children:"NFTs Earned"}),e.jsx(i.Suspense,{fallback:e.jsx("h2",{className:"loading loading-spinner"}),children:e.jsx(y,{account:a})})]}),e.jsx("div",{id:"submissions",className:"mt-4 mb-4 w-11/12 scroll-m-24",children:e.jsx("div",{className:"card bg-secondary/25 h-auto w-full mx-auto rounded-sm",children:e.jsxs("div",{className:"card-body items-center text-center px-12",children:[e.jsx("h2",{className:"card-title",children:"Submission Statuses"}),e.jsx(i.Suspense,{fallback:e.jsx("h2",{className:"loading loading-spinner"}),children:e.jsx(w,{account:a})})]})})}),e.jsx("div",{id:"workshops-attended",className:"mt-4 mb-4 w-11/12 scroll-m-24",children:e.jsx(q,{})}),e.jsx("label",{htmlFor:"my-drawer-2",className:"btn btn-primary drawer-button lg:hidden",children:"Open drawer"})]}),e.jsxs("div",{className:"drawer-side",children:[e.jsx("label",{htmlFor:"my-drawer-2","aria-label":"close sidebar",className:"drawer-overlay"}),e.jsxs("ul",{className:"menu bg-base-200 text-base-content min-w-0",children:[e.jsx("h1",{className:"mt-4 mb-4 text-lg font-secondary",children:r}),e.jsx("li",{children:e.jsx(m,{to:"#nfts-earned",children:"NFTs Earned"})}),e.jsx("li",{children:e.jsx(m,{to:"#submissions",children:"Submissions"})}),e.jsx("li",{children:e.jsx(m,{to:"#workshops-attended",children:"Workshops Attended"})})]})]})]}),e.jsx(f,{})]})}});export{v as Route};
